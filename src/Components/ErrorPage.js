405fc6d1be20a2d9954769c3cf91f545a422d1f0
cmz1212 <chua.ming.zhou.1212@gmail.com>
2023-07-04 21:00:44 +0800

    check

--- src/Components/ErrorPage.js
+++ src/Components/ErrorPage.js
@@ -1,7 +1,7 @@
 export default function ErrorPage() {
   return (
     <div>
-      <h2>I don't think you're meant to get to this page....</h2>
+      <h2>I don't think you're meant to get to this page ....</h2>
       <h1>404 Page not found?</h1>
     </div>
   );
